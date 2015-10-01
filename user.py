f439d322f73da350d3c7d6cf39e2d8f24b247586
Henry Chang <henryc@google.com>
2015-10-01 15:08:32 -0700

    Skeleton server with handlers & datastore services to manage users and their tokens (add, delete, get, list, etc.)  Leaving a fake token authorization in place until we determine what proxy server will do.

---
+++ user.py
@@ -0,0 +1,45 @@
+"""The module for handling users."""
+
+from appengine_config import JINJA_ENVIRONMENT
+from auth import oauth_decorator
+from datastore import User
+from error_handlers import Handle500
+from google.appengine.api import app_identity
+import webapp2
+
+
+def _RenderUserListTemplate():
+  """Render a list of users."""
+  users = User.GetUsers()
+  template_values = {
+      'host': app_identity.get_default_version_hostname(),
+      'users': users
+  }
+  template = JINJA_ENVIRONMENT.get_template('templates/user.html')
+  return template.render(template_values)
+
+
+class ListUsersHandler(webapp2.RequestHandler):
+
+  @oauth_decorator.oauth_required
+  def get(self):
+    self.response.write(_RenderUserListTemplate())
+
+
+class DeleteUserHandler(webapp2.RequestHandler):
+
+  @oauth_decorator.oauth_required
+  def get(self):
+    email = self.request.get('email')
+    User.DeleteUser(email)
+    self.response.write(_RenderUserListTemplate())
+
+
+app = webapp2.WSGIApplication([
+    ('/', ListUsersHandler),
+    ('/user/delete', DeleteUserHandler),
+    (oauth_decorator.callback_path, oauth_decorator.callback_handler()),
+], debug=True)
+
+# This is the only way to catch exceptions from the oauth decorators.
+app.error_handlers[500] = Handle500
