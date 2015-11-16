b8df6c04724fa95b94ffe0555ab2f00352e268c0
Henry Chang <henryc@google.com>
2015-11-16 15:36:38 -0800

    Make the proxy servers to be editable.

--- proxy_server.py
+++ proxy_server.py
@@ -8,13 +8,14 @@ import xsrf
 import webapp2
 
 
-def _RenderAddProxyServerTemplate():
-  """Render the page to add a proxy server."""
+def _RenderProxyServerFormTemplate(proxy_server):
+  """Render the form to add or edit a proxy server."""
   template_values = {
       'host': app_identity.get_default_version_hostname(),
+      'proxy_server': proxy_server,
       'xsrf_token': xsrf.xsrf_token(),
   }
-  template = JINJA_ENVIRONMENT.get_template('templates/add_proxy_server.html')
+  template = JINJA_ENVIRONMENT.get_template('templates/proxy_server_form.html')
   return template.render(template_values)
 
 def _RenderListProxyServerTemplate():
@@ -32,7 +33,7 @@ class AddProxyServerHandler(webapp2.RequestHandler):
 
   @admin.require_admin
   def get(self):
-    self.response.write(_RenderAddProxyServerTemplate())
+    self.response.write(_RenderProxyServerFormTemplate(None))
 
   @admin.require_admin
   @xsrf.xsrf_protect
@@ -42,6 +43,24 @@ class AddProxyServerHandler(webapp2.RequestHandler):
       self.request.get('ssh_private_key'),
       self.request.get('fingerprint'))
     self.redirect('/proxyserver/list')
+
+
+class EditProxyServerHandler(webapp2.RequestHandler):
+
+  @admin.require_admin
+  def get(self):
+    proxy_server = ProxyServer.Get(int(self.request.get('id')))
+    self.response.write(_RenderProxyServerFormTemplate(proxy_server))
+
+  @admin.require_admin
+  @xsrf.xsrf_protect
+  def post(self):
+    ProxyServer.Update(
+      int(self.request.get('id')),
+      self.request.get('ip_address'),
+      self.request.get('ssh_private_key'),
+      self.request.get('fingerprint'))
+    self.response.write(_RenderListProxyServerTemplate())
 
 
 class DeleteProxyServerHandler(webapp2.RequestHandler):
@@ -62,5 +81,6 @@ class ListProxyServersHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/proxyserver/add', AddProxyServerHandler),
     ('/proxyserver/delete', DeleteProxyServerHandler),
+    ('/proxyserver/edit', EditProxyServerHandler),
     ('/proxyserver/list', ListProxyServersHandler),
 ], debug=True)
