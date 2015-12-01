5cbb31ec807a73cbc23203e4a34a6f8333808b20
Ethan Holder <xwsxethan@gmail.com>
2015-12-01 14:47:07 -0800

    Add preliminary support for deploy and release commands in the setup script. Also add some sanity checking to avoid recreating the environment when not necessary.

--- setup.sh
+++ setup.sh
@@ -4,11 +4,12 @@
 # allows script to be run from different directories but always act on the
 # directory it is within.
 ROOT_DIR="$(cd "$(dirname $0)"; pwd)";
+UP_DIR="$(cd ..; pwd)";
 
 # I want to be able to get the latest automatically, but for now I just
 # grab the version that is the latest online. If Google would give latest
 # a static name then we could easily change.
-AE_PYTHON_VERSION="1.9.28";
+AE_PYTHON_VERSION="1.9.30";
 AE_PYTHON_NAME="google_appengine";
 AE_PYTHON_ZIP_NAME="${AE_PYTHON_NAME}_${AE_PYTHON_VERSION}.zip";
 AE_PYTHON_LOCATION="https://storage.googleapis.com/appengine-sdks/featured/";
@@ -26,10 +27,12 @@ AE_PYTHON_WEBAPP="${AE_PYTHON_LIB}webapp2-2.5.2/";
 AE_PYTHON_WEBOB="${AE_PYTHON_LIB}webob-1.2.3/";
 AE_PYTHON_FANCY="${AE_PYTHON_LIB}fancy_urllib/";
 
+AE_PYTHON_UP="$UP_DIR/$AE_PYTHON_NAME/";
+
 UFO_MS_NAME="ufo-management-server-master"
 UFO_MS_GIT_REPO="https://github.com/uProxy/ufo-management-server.git";
 UFO_MS_LOCAL_DIR="$ROOT_DIR/$UFO_MS_NAME/";
-UFO_MS_LOCAL_LIB="$ROOT_DIR/lib";
+UFO_MS_LOCAL_LIB="$UFO_MS_LOCAL_DIR/lib";
 
 TEMP_BASH_PROFILE=".bash_profile";
 
@@ -59,9 +62,16 @@ function addVendorPackage ()
 
 function AddAppEngineRuntimePackages ()
 {
-  runAndAssertCmd "pip install WebTest"
   runAndAssertCmd "pip install Jinja2"
   runAndAssertCmd "pip install pyyaml"
+}
+
+function AddTestingPackages ()
+{
+  runAndAssertCmd "pip install WebTest"
+  runAndAssertCmd "pip install pytest"
+  runAndAssertCmd "pip install nose"
+  runAndAssertCmd "pip install mock"
 }
 
 function addAllExports ()
@@ -72,35 +82,85 @@ function addAllExports ()
   runAndAssertCmd "source $TEMP_BASH_PROFILE"
 }
 
-function setupUnitTest ()
+function setupDevelopmentEnvironment ()
 {
-  setupAppEngine
-  addVendorPackage
-  AddAppEngineRuntimePackages
-  addAllExports
+  if [ ! -d  "$AE_PYTHON_LOCAL_DIR" ] && [ ! -d  "$AE_PYTHON_UP" ]; then
+    setupAppEngine
+    addVendorPackage
+    AddAppEngineRuntimePackages
+    addAllExports
+  else
+    echo "Development environment already setup with appengine and packages."
+  fi
+}
+
+function fixTravisCIEnvironment ()
+{
+  if [ ! -d  "$UFO_MS_LOCAL_DIR" ] && [ ! -d  "$AE_PYTHON_UP" ]; then
+    TEMP_FILES="$(ls -A)"
+    runAndAssertCmd "mkdir $UFO_MS_LOCAL_DIR"
+    runAndAssertCmd "cp $TEMP_FILES $UFO_MS_LOCAL_DIR -r"
+    runAndAssertCmd "rm -fr $TEMP_FILES"
+  else
+    echo "Code files already in proper directory."
+  fi
+}
+
+function setupUnitTests ()
+{
+  fixTravisCIEnvironment
+  setupDevelopmentEnvironment
+  AddTestingPackages
+}
+
+function runUnitTests ()
+{
+  setupUnitTests
+  runAndAssertCmd "python -m unittest discover -p '*_test.py'"
+}
+
+function release ()
+{
+  runUnitTests
+  #package
+  #push
+}
+
+function deploy ()
+{
+  runAndAssertCmd "$AE_PYTHON_LOCAL_DIR/appcfg.py -A uproxy-management-server update $UFO_MS_LOCAL_DIR"
 }
 
 function setupManagementServer ()
 {
   runAndAssertCmd "git clone $UFO_MS_GIT_REPO"
-  runAndAssertCmd "cp $UFO_MS_LOCAL_DIR/* . -r"
-  runAndAssertCmd "rm -fr $UFO_MS_LOCAL_DIR"
-  setupUnitTest
+}
+
+function installManagementServer ()
+{
+  setupManagementServer
+  setupDevelopmentEnvironment
 }
 
 if [ "$1" == 'install' ]; then
-  setupManagementServer
-elif [ "$1" == 'unit_test' ]; then
-  setupUnitTest
-elif [ "$1" == 'add_exports' ]; then
-  addAllExports
+  installManagementServer
+elif [ "$1" == 'release' ]; then
+  release
+elif [ "$1" == 'deploy' ]; then
+  deploy
+elif [ "$1" == 'run_tests' ]; then
+  runUnitTests
+elif [ "$1" == 'setup_tests' ]; then
+  setupUnitTests
 else
   echo
-  echo "Usage: setup.sh [install|unit_test]"
+  echo "Usage: setup.sh [install|release|deploy|run_tests|setup_tests]"
   echo
   echo "  install      - Sets up the entire project from github."
-  echo "  unit_test    - Prepares the machine for unit testing."
-  echo "  add_exports  - Add environment exports for variable setup."
+  echo "  release      - Runs the tests and uploads a zip if successful."
+  echo "  deploy       - Uploads the local code copy to appspot."
+  echo "  run_tests    - Prepares the machine for unit testing and runs."
+  echo "  setup_tests  - Prepares the machine for unit testing."
   echo
   exit 0
 fi
