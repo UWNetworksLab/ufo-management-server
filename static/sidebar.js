6f29b19e123fa4aa2d7c6952b2426f288d138745
Jonathan Pevarnek <jpevarnek@google.com>
2015-12-01 16:51:53 -0500

    Start using Polymer

---
+++ static/sidebar.js
@@ -0,0 +1,23 @@
+Polymer({
+  is: "ufo-sidebar",
+  ready: function() {
+    this.pages = [
+    {"href": "/", "text": "Home"},
+    {"href": "/user", "text": "Users"},
+    {"href": "/user/listTokens", "text": "Tokens"},
+    {"href": "/proxyserver/list", "text": "Proxy Servers"},
+    {"href": "/setup/oauthclient", "text": "Setup"},
+    {"href": "/logout", "text": "Logout"}
+    ];
+  },
+  itemClicked: function(e) {
+    window.location.href = e.target.getAttribute("data-href");
+  },
+  currentPage: function(pages) {
+    for (var i in pages) {
+      if (pages[i].href == window.location.pathname) {
+        return i;
+      }
+    }
+  }
+});
