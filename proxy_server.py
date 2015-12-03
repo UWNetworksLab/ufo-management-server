f9181a4a0490d3ed0a43015ee1cc5c29d90063d5
Henry Chang <henryc@henryc-macbookair3.roam.corp.google.com>
2015-12-03 15:51:45 +0800

    Fix the cron job.

--- proxy_server.py
+++ proxy_server.py
@@ -111,10 +111,6 @@ class ListProxyServersHandler(webapp2.RequestHandler):
 
 class DistributeKeyHandler(webapp2.RequestHandler):
 
-  # This is accessed by the cron service, which only has appengine admin access.
-  # So if the admin check changes here, we might have to move this to a
-  # different module.
-  @admin.require_admin
   def get(self):
     # TODO(henry): See if we can use threading to parallelize the put requests.
     key_string = _MakeKeyString()
@@ -138,7 +134,8 @@ class DistributeKeyHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/proxyserver/add', AddProxyServerHandler),
     ('/proxyserver/delete', DeleteProxyServerHandler),
-    ('/proxyserver/distributekey', DistributeKeyHandler),
     ('/proxyserver/edit', EditProxyServerHandler),
     ('/proxyserver/list', ListProxyServersHandler),
+
+    ('/cron/proxyserver/distributekey', DistributeKeyHandler),
 ], debug=True)
