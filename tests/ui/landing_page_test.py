9c0d96156ede8de350f306ae9e65c8e466771e5a
Henry Chang <henryc@henryc-macbookair2.roam.corp.google.com>
2015-12-08 11:04:47 -0800

    convert indents to spaces

--- tests/ui/landing_page_test.py
+++ tests/ui/landing_page_test.py
@@ -9,20 +9,20 @@ from selenium import webdriver
 class LandingPageTest(unittest.TestCase):
 
   def setUp(self):
-	self.driver = webdriver.Chrome('../../chromedriver')
-	DevServerLoginPage(self.driver).Login()
+    self.driver = webdriver.Chrome('../../chromedriver')
+    DevServerLoginPage(self.driver).Login()
 
   def testLandingPage(self):
-  	title = 'UfO Management Server'
-  	instruction = ('Click one of the links on the side to login and '
-  				   'administer the server.')
+    title = 'UfO Management Server'
+    instruction = ('Click one of the links on the side to login and '
+                   'administer the server.')
 
-	self.driver.get('http://localhost:9999')
+    self.driver.get('http://localhost:9999')
 
-	landing_page = LandingPage(self.driver)
-	self.assertEquals(title, landing_page.GetTitle().text)
-	self.assertEquals(instruction, landing_page.GetInstruction().text)
-	self.assertIsNotNone(landing_page.GetSidebar())
+    landing_page = LandingPage(self.driver)
+    self.assertEquals(title, landing_page.GetTitle().text)
+    self.assertEquals(instruction, landing_page.GetInstruction().text)
+    self.assertIsNotNone(landing_page.GetSidebar())
 
   def tearDown(self):
     self.driver.quit()
