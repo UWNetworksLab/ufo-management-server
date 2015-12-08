750a90919da428d2ca2cebbc67167f60212e469c
Henry Chang <henryc@henryc-macbookair2.roam.corp.google.com>
2015-12-08 11:05:51 -0800

    more indents to spaces

--- tests/ui/user_page_test.py
+++ tests/ui/user_page_test.py
@@ -9,18 +9,18 @@ from selenium import webdriver
 class UserPageTest(unittest.TestCase):
 
   def setUp(self):
-	self.driver = webdriver.Chrome('../../chromedriver')
-	DevServerLoginPage(self.driver).Login()
+  self.driver = webdriver.Chrome('../../chromedriver')
+  DevServerLoginPage(self.driver).Login()
 
   def testUserPage(self):
-  	add_users = 'Add Users'
-  	list_tokens = 'List Tokens'
+    add_users = 'Add Users'
+    list_tokens = 'List Tokens'
 
-  	self.driver.get('http://localhost:9999/user')
-	user_page = UserPage(self.driver)
-	self.assertEquals(add_users, user_page.GetAddUserLink().text)
-	self.assertEquals(list_tokens, user_page.GetListTokensLink().text)
-	self.assertIsNotNone(user_page.GetSidebar())
+    self.driver.get('http://localhost:9999/user')
+  user_page = UserPage(self.driver)
+  self.assertEquals(add_users, user_page.GetAddUserLink().text)
+  self.assertEquals(list_tokens, user_page.GetListTokensLink().text)
+  self.assertIsNotNone(user_page.GetSidebar())
 
   def tearDown(self):
     self.driver.quit()
