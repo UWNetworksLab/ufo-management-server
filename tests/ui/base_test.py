aafca4465fe525feb7a74f0270817327e0059fea
Ethan Holder <xwsxethan@gmail.com>
2015-12-15 11:12:54 -0800

    Hopefully the final commit necessary to clean up our health checks!

--- tests/ui/base_test.py
+++ tests/ui/base_test.py
@@ -7,5 +7,6 @@ class BaseTest(unittest.TestCase):
   """Base test class to inherit from."""
 
   def __init__(self, methodName='runTest', args=None):
+    """Create the base test object for others to inherit."""
     super(BaseTest, self).__init__(methodName)
     self.args = args
