738dc153cc1579e09c7a1dc4b6da324c46ad11ff
Henry Chang <henryc@google.com>
2015-12-15 10:18:01 -0800

    organize the imports

--- tests/ui/test_config.py
+++ tests/ui/test_config.py
@@ -6,12 +6,12 @@ sys.path.append('../..')
 
 # pylint: disable=unused-import
 from config import LANDING_PAGE_PATH
-from config import USER_PAGE_PATH
+from config import USER_ADD_PATH
 from config import USER_DELETE_PATH
+from config import USER_DETAILS_PATH
 from config import USER_GET_INVITE_CODE_PATH
 from config import USER_GET_NEW_KEY_PAIR_PATH
-from config import USER_ADD_PATH
+from config import USER_PAGE_PATH
 from config import USER_TOGGLE_REVOKED_PATH
-from config import USER_DETAILS_PATH
 
 CHROME_DRIVER_LOCATION = '../../lib/chromedriver'
