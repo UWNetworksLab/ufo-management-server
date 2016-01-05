eae3b8046138a3d881b3e66bdcc5dc59470be4ae
Ethan Holder <xwsxethan@gmail.com>
2016-01-05 11:08:58 -0800

    Adding regex pattern matching to the paper inputs in order to validate input text matches what we expect.

--- appengine_config.py
+++ appengine_config.py
@@ -20,6 +20,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 JINJA_ENVIRONMENT.globals['xsrf_token'] = xsrf.XSRFToken()
 HOST = str(app_identity.get_default_version_hostname())
 JINJA_ENVIRONMENT.globals['BASE_URL'] = ('https://' + HOST)
+JINJA_ENVIRONMENT.globals['EMAIL_VALIDATION_PATTERN'] = r'[^@]+@[^@]+.[^@]+'
+email_validation_error = 'Please supply a valid email address.'
+JINJA_ENVIRONMENT.globals['EMAIL_VALIDATION_ERROR'] = email_validation_error
+# Key lookup for users and group allows email or unique id.
+key_lookup_pattern = r'([^@]+@[^@]+.[^@]+|[a-zA-Z0-9]+)'
+key_lookup_error = 'Please supply a valid email address or unique id.'
+JINJA_ENVIRONMENT.globals['KEY_LOOKUP_VALIDATION_PATTERN'] = key_lookup_pattern
+JINJA_ENVIRONMENT.globals['KEY_LOOKUP_VALIDATION_ERROR'] = key_lookup_error
 
 for key, value in PATHS.iteritems():
   JINJA_ENVIRONMENT.globals[key] = value
